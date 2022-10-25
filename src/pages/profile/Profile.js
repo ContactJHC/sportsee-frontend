c025aac9d3d76b40278052ab8be4ca23cb5229a4
ContactJHC <95849631+ContactJHC@users.noreply.github.com>
2022-10-25 20:14:23 +0200

    containers vides pour charts & summaries

--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -3,8 +3,18 @@ import Banner from '../../components/banner/Banner'
 
 export default function Profile() {
   return (
-    <div id='bannerContainer'>
-        <Banner userName={'Thomas'}/>
+    <div id='mainContainer'>
+        <div id='bannerContainer'>
+            <Banner userName={'Thomas'}/>
+        </div>
+        <div id='chartsAndSummariesContainer'>
+            <div id='chartsContainer'>
+
+            </div>
+            <div id='summariesContainer'>
+
+            </div>
+        </div>
     </div>
   )
 }
