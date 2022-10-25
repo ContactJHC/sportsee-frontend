a8a0b46202ee28ca53dbe81df57b89cf0ce68979
ContactJHC <95849631+ContactJHC@users.noreply.github.com>
2022-10-25 17:39:01 +0200

    menu vertical

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 // import logo from './logo.svg';
 // import './App.css';
-import Header from './components/Header/Header';
+import Header from './components/Header/Header'
+import Menu from './components/Menu/Menu'
+
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Menu />
     </div>
   );
 }
---
+++ src/components/Menu/Menu.js
@@ -0,0 +1,21 @@
+import './Menu.css'
+import meditation from '../../assets/meditation.png'
+import swimming from '../../assets/swimming.png'
+import biking from '../../assets/biking.png'
+import workout from '../../assets/workout.png'
+
+export default function Menu() {
+  return (
+    <div id='lateralMenu'>
+        <div id='lateralIcons'>
+            <img src={meditation} alt='meditation' />
+            <img src={swimming} alt='swimming' />
+            <img src={biking} alt='biking' />
+            <img src={workout} alt='workout' />
+        </div>
+        <div id='lateralCopyright'>
+            <p>Copyright, SportSee 2020</p>
+        </div>
+    </div>
+  )
+}
