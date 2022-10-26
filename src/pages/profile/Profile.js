6e7c84d2a14bb8a1fca71e3282aae1cce81149bf
ContactJHC <95849631+ContactJHC@users.noreply.github.com>
2022-10-26 10:20:03 +0200

    charts and summaries containers

--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -9,10 +9,20 @@ export default function Profile() {
         </div>
         <div id='chartsAndSummariesContainer'>
             <div id='chartsContainer'>
+              <div id='dailyActivityContainer'>
 
+              </div>
+              <div id='averageAndIntensityAndScoreContainer'>
+                <div id='averageChart'></div>
+                <div id='intensityChart'></div>
+                <div id='scoreChart'></div>
+              </div>
             </div>
             <div id='summariesContainer'>
-
+              <div id='caloriesSummary'></div>
+              <div id='proteinsSummary'></div>
+              <div id='carbsSummary'></div>
+              <div id='lipidsSummary'></div>
             </div>
         </div>
     </div>
