4914d0846d4b81255aff16ff48abf95c84e356de
ContactJHC <95849631+ContactJHC@users.noreply.github.com>
2022-12-27 06:47:36 +0100

    app.js : cleaning useless code

--- src/App.js
+++ src/App.js
@@ -9,14 +9,6 @@ function App() {
       <Header />
       <Menu />
       <Profile />
-      {/* <GetData /> */}
-      {/* Ci-dessous, les divz paramétrées pour afficher les diagrammes */}
-      {/* <div style={{backgroundColor:'#FBfbFb', width:'500px', height:'500px', border:'solid red 10px', position:'absolute', top:'150px', left:'150px'}}>
-        <Spinner />
-      </div> */}
-      {/* <div style={{width:'500px', height:'500px', border:'solid red 10px', position:'absolute', top:'150px', left:'700px'}}>
-        <ChartScoreModel />
-      </div> */}
     </div>
   );
 }
